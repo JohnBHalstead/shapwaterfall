69020862876cf57c8234120762804a9be34c9e3a
John Halstead <50117154+JohnBHalstead@users.noreply.github.com>
2020-09-30 11:07:09 -0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="shapwaterfall",     
+    version="0.1.0",
+    author="John Halstead",
+    author_email="jhalstead@vmware.com",
+    description="A SHAP Waterfall Chart interpreting local differences between observations",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/JohnBHalstead/shapwaterfall",
+    install_requires=[            
+          'shap',
+          'waterfall_chart',
+          'numpy',
+          'matplotlib.pyplot',
+          'pandas'
+      ],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
